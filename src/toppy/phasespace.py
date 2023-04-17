8f5010299235b725c0dd01ea73ab4a5a5d1c4398
icmeyer <isaacmeyer0@gmail.com>
2023-04-17 14:11:10 -0400

    plot energy spectra

--- src/toppy/phasespace.py
+++ src/toppy/phasespace.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib import colors
-
+import os
 
 from ParticlePhaseSpace import DataLoaders
 from ParticlePhaseSpace import PhaseSpace
@@ -10,6 +10,14 @@ from ParticlePhaseSpace import PhaseSpace
 # https://www.python-graph-gallery.com/86-avoid-overlapping-in-scatterplot-with-2d-density
 # Acknowledgement to plotting in ParticlePhaseSpace:
 # https://github.com/bwheelz36/ParticlePhaseSpace/blob/main/ParticlePhaseSpace/_ParticlePhaseSpace.py
+
+pdg2particle = {11: 'electrons',
+                -11: 'positrons',
+                2212: 'protons',
+                22: 'gammas',
+                2112: 'neutrons',
+                0: 'optical_photons'}
+particle2pdg = {v: k for k, v in pdg2particle.items()}
 
 def make_histogram(weights, x_data, y_data, xgrid, ygrid):
     h, xedges, yedges = np.histogram2d(x_data, y_data,
@@ -24,9 +32,18 @@ def plot_hist_2d(ax, histogram, norm):
     ax.set_aspect('equal')
     return pcm
 
-        
+def plot_hist_1d(ax, xmin, xmax, nbins, data, xlabel, title=None):
+    histbins = np.linspace(xmin, xmax, nbins + 1)
+    counts, bins = np.histogram(data, bins=histbins)
+    # Normalize the counts (probability mass function)
+    counts = counts/np.sum(counts)
+    ax.stairs(counts, bins)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel('Probability Mass Function')
+    if title is not None:
+        ax.set_title(title)
 
-def custom_pairplot(df, variables):
+def custom_pairplot(df, variables, title, filename):
     fig = plt.figure(figsize=(12,9))
     axd = fig.subplot_mosaic(
         """
@@ -48,15 +65,8 @@ def custom_pairplot(df, variables):
                 variable_tuples.append([variables[i],variables[j]])
 
     nbins = 40
-    histbins = np.linspace(-1, 1, nbins)
     for i, diag in enumerate(diags):
-        # axd[diag].hist(df[variables[i]], bins=nbins)
-        counts, bins = np.histogram(df[variables[i]], bins=histbins)
-        # Normalize the counts (probability mass function)
-        counts = counts/np.sum(counts)
-        axd[diag].stairs(counts, bins)
-        axd[diag].set_xlabel(variables[i])
-        axd[diag].set_ylabel('Probability Mass Function')
+        plot_hist_1d(axd[diag], -1, 1, nbins, df[variables[i]], variables[i])
 
     nbins = 20
     xlim = [-1, 1]
@@ -85,21 +95,64 @@ def custom_pairplot(df, variables):
     cbar = fig.colorbar(pcm, cax=axd['j'])
     cbar.set_label('Probability Mass Function (values sum to 1)')
 
+    fig.suptitle(title)
     fig.tight_layout()
 
+    fig.savefig(filename + '.pdf', dpi=400)
 
-if __name__=='__main__':
-    phsp_file = '/Users/isaacmeyer/research/sarrp/SARRP_topas/4DHead/ASCIIOutput.phsp'
+def plot_energy_spectra(df, particles, filename, nbins=100):
+    fig = plt.figure(figsize = [8, 4])
+
+    for i, particle in enumerate(particles):
+        ax = fig.add_subplot(1, len(particles), i+1)
+        pdg = particle2pdg[particle]
+        dfpart = df[df['particle type [pdg_code]'] == pdg]
+        nparts = len(dfpart)
+        title = '{:s} Energy Distribution \n'.format(particle)
+        title += '{:.1e} Particles \n'.format(nparts)
+        print(pdg)
+        print(dfpart.columns)
+        print(dfpart)
+        energies = dfpart['Ek [MeV]']
+        xmin = np.amin(energies)
+        xmax = np.amax(energies)
+        plot_hist_1d(ax, xmin, xmax, nbins, energies, 'Kinetic Energy [MeV]')
+        ax.set_title(title)
+    
+    fig.tight_layout()
+    fig.savefig(filename + '.pdf', dpi=400)
+    
+def plot_dists(phsp_file, figdir):
+    os.system('mkdir -p {:s}'.format(figdir))
+    
     phsp_obj = PhaseSpace(DataLoaders.Load_TopasData(phsp_file))
     phsp_obj.fill.direction_cosines()
+    phsp_obj.fill.kinetic_E()
+
+    # Photon plotting
+    df = phsp_obj.ps_data
+
+    # get partricles in dataset
+    pdgs = list(set(df['particle type [pdg_code]'].to_numpy()))
+    particles = [pdg2particle[pdg] for pdg in pdgs]
 
     cosine_names = ['Direction Cosine X', 
                     'Direction Cosine Y',
                     'Direction Cosine Z']
+    for particle in particles:
+        pdg = particle2pdg[particle]
+        dfpart = df[df['particle type [pdg_code]'] == pdg]
+        nparts = len(dfpart)
+        title = '{:s} Cosine Distribution Correlations \n'.format(particle)
+        title += '{:.1e} Particles \n'.format(nparts)
+        filename = figdir + '{:s}_cosines'.format(particle)
+        custom_pairplot(dfpart, cosine_names, title, filename)
+
+    filename = figdir + 'energy_distributions'
+    plot_energy_spectra(df, particles, filename)
 
 
-    # Photon plotting
-    df = phsp_obj.ps_data
-    df = df[df['particle type [pdg_code]'] == 22]
-    custom_pairplot(df, cosine_names)
-    plt.show()
+
+if __name__=='__main__':
+    phsp_file = '/Users/isaacmeyer/research/sarrp/SARRP_topas/4DHead/ASCIIOutput.phsp'
+    plot_dists(phsp_file, 'tests/')
